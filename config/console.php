91c1bf598789a8f843f31897cc394526b17ea23c
Tõnis Ormisson <tonis@andmemasin.eu>
2017-05-11 14:47:33 +0300

    fix timezone5

--- config/console.php
+++ config/console.php
@@ -6,7 +6,10 @@ $db = require(__DIR__ . '/db.php');
 $config = [
     'id' => 'basic-console',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'bootstrap' => [
+        'log',
+        'app\bootstraps\AppBootstrap',
+    ],
     'controllerNamespace' => 'app\commands',
     'components' => [
         'cache' => [

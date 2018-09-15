ecc72e65841c4d20c825f0b6d7d03dfa7739369c
Tõnis Ormisson <tonis@andmemasin.eu>
2018-09-15 10:18:51 +0300

    Merge remote-tracking branch 'upstream/master' into dev-ctrlr-modules
    # Conflicts:
    #	CHANGELOG.md

---
+++ yii2-usuario-project/config/aliases.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    '@bower' => '@vendor/bower-asset',
+    '@npm'   => '@vendor/npm-asset',
+    '@2amigos/yii2-usuario' => __DIR__.'/yii2-usuario/src/User',
+    '@tests' => '@app/tests',
+];

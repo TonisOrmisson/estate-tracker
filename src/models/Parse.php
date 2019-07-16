9245d6c7dfee2c6b6b19259c517fa2da72ae85f9
Tõnis Ormisson <tonis@andmemasin.eu>
2019-07-17 00:15:40 +0300

    use regex instead for 2m extract

--- src/models/Parse.php
+++ src/models/Parse.php
@@ -112,9 +112,8 @@ class Parse extends \yii\db\ActiveRecord
             Yii::info("looking for m2node found for $item->primaryKey: ", __METHOD__);
             $m2 = trim($m2Node->textContent);
             Yii::info("initial m2node value for $item->primaryKey: " . $m2, __METHOD__);
-            $m2 = str_replace(" EUR/m²", "", $m2);
-            $m2 = str_replace(" ", "", $m2);
-            $m2 = str_replace(",", ".", $m2);
+            preg_match_all('/\d+/', $m2, $matches);
+            $m2 = $matches[0];
             $m2 = floatval($m2);
             Yii::info("extracted m2 value for $item->primaryKey: " . $m2, __METHOD__);
         }

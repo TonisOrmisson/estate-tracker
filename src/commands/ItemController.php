0cf791a49eac16ce95067240f71bbc36c6e1a4c9
Tõnis Ormisson <tonis@andmemasin.eu>
2019-07-17 08:02:41 +0300

    activate all by console

---
+++ src/commands/ItemController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace app\commands;
+
+
+use app\models\Item;
+use yii\console\Controller;
+use yii\helpers\Console;
+
+class ItemController extends Controller
+{
+    public function actionActivateAll() {
+        $this->stdout("Activating all items" . PHP_EOL, Console::BOLD, Console::FG_GREEN);
+        $result = (new Item())->update(false,['active' => 1]);
+        $this->stdout("done $result records" . PHP_EOL, Console::BOLD, Console::FG_GREEN);
+    }
+
+}

e3fed20b862bec6844656962d0f5a978fbeeda20
Tõnis Ormisson <tonis@andmemasin.eu>
2019-07-17 08:04:29 +0300

    activate all by console, fix

--- src/commands/ItemController.php
+++ src/commands/ItemController.php
@@ -12,7 +12,7 @@ class ItemController extends Controller
 {
     public function actionActivateAll() {
         $this->stdout("Activating all items" . PHP_EOL, Console::BOLD, Console::FG_GREEN);
-        $result = (new Item())->update(false,['active' => 1]);
+        $result = Item::updateAll(['active' => 1]);
         $this->stdout("done $result records" . PHP_EOL, Console::BOLD, Console::FG_GREEN);
     }
 

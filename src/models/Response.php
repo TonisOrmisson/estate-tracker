465ee667d8a651c761b71bfa3b168fbef60eacb6
Tonis Ormisson <tonis@andmemasin.eu>
2021-05-21 20:00:58 +0300

    fix user-agent, update deps

--- src/models/Response.php
+++ src/models/Response.php
@@ -15,17 +15,19 @@ class Response extends Model
 {
     public static function getResponse($url)
     {
+        $agent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)";
         $options = array(
             CURLOPT_RETURNTRANSFER => true,   // return web page
             CURLOPT_HEADER         => false,  // don't return headers
             CURLOPT_FOLLOWLOCATION => true,   // follow redirects
             CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
             CURLOPT_ENCODING       => "",     // handle compressed
-            CURLOPT_USERAGENT      => "test", // name of client
             CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
             CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
             CURLOPT_TIMEOUT        => 120,    // time-out on response
+            CURLOPT_USERAGENT => $agent,
         );
+
         $ch = curl_init($url);
         curl_setopt_array($ch, $options);
         $content  = curl_exec($ch);
